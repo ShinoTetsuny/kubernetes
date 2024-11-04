9955acfedeae972bfe8d68f8b0c5f74ef6d2fa84
tom <tom.ollivier01@gmail.com>
2024-11-04 14:53:41 +0100

    testa

---
+++ script/webhook.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Récupérer l'URL publique de Ngrok
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+# Modifie l'URL pour le point de terminaison du webhook
+WEBHOOK_URL="${NGROK_URL}/inspect/http"
+
+# Configuration du webhook GitHub
+REPO="https://github.com/ShinoTetsuny/kubernetes"  # Remplacez par votre nom d'utilisateur et dépôt
+TOKEN=""        # Remplacez par votre token GitHub
+
+# Supprimer le webhook existant (optionnel)
+curl -X DELETE -H "Authorization: token $TOKEN" \
+  "https://api.github.com/repos/$REPO/hooks"
+
+# Créer le webhook GitHub
+curl -X POST -H "Authorization: token $TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/$REPO/hooks \
+  -d "{
+    \"config\": {
+      \"url\": \"$WEBHOOK_URL\",
+      \"content_type\": \"json\",
+      \"insecure\": true
+    },
+    \"events\": [
+      \"push\"
+    ],
+    \"active\": true
+  }"
+
+echo "Webhook configuré avec succès à l'adresse : $WEBHOOK_URL"
